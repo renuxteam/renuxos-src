26d7470d2cea948a51fc5885a6e6790d9b0771cf
Renan Lucas <renanluscad@gmail.com>
2025-06-14 19:50:10 +0000

    Delete renuxos-src directory

--- renuxos-src/kernel/boot/entry.s
+++
@@ -1,7 +0,0 @@
-.section .text # text section
-.global _start # make _start global
-.extern _zig_start # declare the Zig start function
-_start:
-    call _zig_start # call the Zig start function
-.hang:
-    jmp .hang # infinite loop to hang the kernel
