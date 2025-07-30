93d0fad6cc5e8fe0c24adb2f33d33bbd329b3ed5
Renan Lucas <renanluscad@gmail.com>
2025-07-30 17:17:16 -0300

    Delete kernel/init/start.zig

--- kernel/init/start.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std"); // Import Zig’s standard library (available for future use)
-const main = @import("main.zig"); // Bring in your kernel’s main module
-
-// Low-level entry point symbol recognized by the linker/bootloader
-export fn _start() void {
-    main.kernel_main(); // Jump into the kernel’s main function
-}
