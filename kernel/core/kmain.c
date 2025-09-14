3c8fb811566dd1d3ff0088aeba219920b3ece0cc
Renan2010 <renanluscad@gmail.com>
2025-09-14 17:04:26 -0300

    Update Files

--- kernel/core/kmain.c
+++ kernel/core/kmain.c
@@ -3,11 +3,23 @@
 #include "../../drivers/video/include/color.h"
 #include "../../drivers/video/include/framebuffer.h"
 
+const char *ascii_logo = R"(
+ _____                        ____   _____ 
+|  __ \                      / __ \ / ____|
+| |__) |___ _ __  _   ___  _| |  | | (___  
+|  _  // _ \ '_ \| | | \ \/ / |  | |\___ \ 
+| | \ \  __/ | | | |_| |>  <| |__| |____) |
+|_|  \_\___|_| |_|\__,_/_/\_\\____/|_____/ 
+)";
+
 
 void kmain(void) {
     fb_init();
     console_init();
     console_clear();
-    fb_clear(COLOR_GRAY);
-    console_write("Hello RenuxOS");
+    fb_clear(COLOR_BLACK);
+    console_set_text_color(COLOR_RED);
+    console_write(ascii_logo);
+    console_putchar('\n');
+    console_write("RenuxOS v0.1 Aurora \n");
 }
